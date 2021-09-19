9be60d5b38f3242146b32d94f8a7afa91cab6558
gihal <gihal83@gmail.com>
2021-09-19 13:00:58 +1200

    Tidy up the code by moving some business operations to the service

--- auction-service/src/main/java/com/tsb/auctionservice/controller/AuctionController.java
+++ auction-service/src/main/java/com/tsb/auctionservice/controller/AuctionController.java
@@ -2,87 +2,67 @@ package com.tsb.auctionservice.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.tsb.auctionservice.service.ParamSortOption;
+import com.tsb.auctionservice.controller.service.AuctionService;
 import common.datetime.DateTimeParser;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Iterator;
+import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 @RestController
 public class AuctionController {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private AuctionService service;
 
-    @Autowired
-    private HttpEntity<String> httpEntity;
-
-
-    @GetMapping(value = "/auction")
-    public ResponseEntity<Object> getAuctions(
+    @GetMapping(value = "/auction", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<JsonNode>> getAuctions(
             @RequestParam(name = "categoryName", required = false) final String categoryName,
             @RequestParam(name = "sortOption", required = false) final String sortOption) {
-        String url = "https://api.tmsandbox.co.nz/v1/Search/General.json?listed_as=Auctions&depth=0";
+        String url = "https://api.tmsandbox.co.nz/v1/Search/General.json?listed_as=Auctions";
         /*if (categoryName != null && !categoryName.isEmpty()) {
             url += "&category=" + categoryName;
         }
-        if (sortOrder != null && !sortOrder.isEmpty()) {
-            url += "&sort_order=" + sortOrder;
+        if (sortOption != null && !sortOption.isEmpty()) {
+            url += "&sort_order=" + sortOption;
         }*/
-
-
-        ResponseEntity<String> exchange = restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
-        ObjectMapper objectMapper = new ObjectMapper();
         try {
-
-            Iterator<JsonNode> elements = objectMapper.readTree(exchange.getBody()).get("List").elements();
-            Iterable<JsonNode> iterable = () -> elements;
-            List<JsonNode> list = StreamSupport
-                    .stream(iterable.spliterator(), false)
-                    .filter(jsonNode ->
-                            (categoryName == null || jsonNode.get("Category").asText().startsWith(categoryName))
-                                    && (jsonNode.get("HasReserve") == null || !jsonNode.get("HasReserve").asBoolean())
-                    )
-                    .collect(Collectors.toList());
-
-            if (sortOption != null) {
-                if (ParamSortOption.Title.toString().equals(sortOption)) {
-                    list.sort(
-                            (JsonNode node1, JsonNode node2) -> node1.get("Title").textValue().compareTo(node2.get("Title").textValue())
-                    );
-                } else if (ParamSortOption.ExpiryAsc.toString().equals(sortOption)) {
-                    list.sort(
-                            (JsonNode node1, JsonNode node2) -> DateTimeParser.parseDate(node1.get("EndDate").textValue()).compareTo(DateTimeParser.parseDate(node2.get("EndDate").textValue()))
-                    );
-                } else if (ParamSortOption.ExpiryDesc.toString().equals(sortOption)) {
-                    list.sort(
-                            (JsonNode node1, JsonNode node2) -> DateTimeParser.parseDate(node2.get("EndDate").textValue()).compareTo(DateTimeParser.parseDate(node1.get("EndDate").textValue()))
-                    );
-                } else if (ParamSortOption.PriceAsc.toString().equals(sortOption)) {
-                    list.sort(
-                            (JsonNode node1, JsonNode node2) -> node1.get("StartPrice").decimalValue().compareTo(node2.get("StartPrice").decimalValue())
-                    );
-                } else if (ParamSortOption.PriceDesc.toString().equals(sortOption)) {
-                    list.sort(
-                            (JsonNode node1, JsonNode node2) -> node2.get("StartPrice").decimalValue().compareTo(node1.get("StartPrice").decimalValue())
-                    );
-                }
-
-            }
-
+            List<JsonNode> list = service.getNoReserveTradeAuction(url, categoryName, sortOption);
             return ResponseEntity.ok().body(list);
-        } catch (JsonProcessingException e) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error on processing data...");
+        } catch (JsonProcessingException jsonProcessingException) {
+            jsonProcessingException.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error on processing data.");
+        } catch (HttpServerErrorException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
     }
+
+    @GetMapping("/auction/expiring")
+    public ResponseEntity<List<JsonNode>> getExpiringAuction(
+            @RequestParam(name = "startTime") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm") Date startTime,
+            @RequestParam(name = "endTime") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm") Date endTime,
+            @RequestParam(name = "categoryName", required = false) String categoryName,
+            @RequestParam(name = "sortOption", required = false) String sortOption) {
+        String startTimeStr = DateTimeParser.formatDate(startTime);
+        String url = "https://api.tmsandbox.co.nz/v1/Search/General.json?listed_as=Auctions&date_from=" + startTimeStr;
+        try {
+            List<JsonNode> list = service.getExpiringAuctions(url, endTime, categoryName, sortOption);
+            return ResponseEntity.ok().body(list);
+        } catch (JsonProcessingException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error on processing data.");
+        } catch (HttpServerErrorException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
+
 }
