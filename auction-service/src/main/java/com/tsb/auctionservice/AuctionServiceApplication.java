48a851e4d15e490056506dbfdfecd2e24e2bd1c2
gihal <gihal83@gmail.com>
2021-09-19 20:32:59 +1200

    Integrated swagger

--- auction-service/src/main/java/com/tsb/auctionservice/AuctionServiceApplication.java
+++ auction-service/src/main/java/com/tsb/auctionservice/AuctionServiceApplication.java
@@ -1,5 +1,9 @@
 package com.tsb.auctionservice;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -38,6 +42,17 @@ public class AuctionServiceApplication {
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
         return headers;
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption, @Value("${application-version}") String appVersion) {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Auction Client API")
+                        .version(appVersion)
+                        .description(appDesciption)
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
 
 }
